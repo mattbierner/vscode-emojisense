88c2094db001c3902dd269f32ed4a4851184e6f4
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-03-28 22:50:26 +0100

    Attempt to discriminate between activeEditor and terminal :male_detective:

--- src/quickEmoji.ts
+++ src/quickEmoji.ts
@@ -4,15 +4,20 @@ import {EmojiProvider, Emoji} from './emoji'
 export const quickEmoji = (emoji: EmojiProvider) => (property: keyof Emoji) => {
 	const items: QuickPickItem[] = Array.from(emoji.emojis).map(
 		emoji =>({label: emoji.name, description: emoji.emoji})
-		);
+	);
+	let terminalCurrent = false;
+	window.onDidChangeActiveTerminal(() => { terminalCurrent = true;})
+	window.onDidChangeActiveTextEditor(() => { terminalCurrent = false;})
+
 	return async function () {
+		const terminal = window.activeTerminal;
+		const editor = window.activeTextEditor;
 		const emoji = await window.showQuickPick(items);
 		if (!emoji) return;
 		const insert = property === 'name' ? `:${emoji.label}`: emoji.description || '';
-		if (window.activeTextEditor) {
-			const editor = window.activeTextEditor
+		if (terminal && terminalCurrent) terminal.sendText(insert, false);
+		else if (editor) {
 			editor.edit(builder => builder.insert(editor.selection.active, insert));
 		}
-		else if (window.activeTerminal) window.activeTerminal.sendText(insert, false);
 	}
 };
