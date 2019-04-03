5cc763f6d4271fa3d92694b0bcbda1b613ed712b
AdrieanKhisbe <adriean.khisbe@live.fr>
2019-04-03 07:35:45 +0200

    Tweaks to the picker to swap emoji and text, have title :hammer_and_wrench:

--- src/quickEmoji.ts
+++ src/quickEmoji.ts
@@ -1,20 +1,40 @@
 import { Uri, window, Disposable, QuickPickItem, workspace, TextEdit } from 'vscode';
-import {EmojiProvider, Emoji} from './emoji'
+import { EmojiProvider, Emoji } from './emoji'
 
 export type EmojiDestination = 'terminal' | 'editor';
-export const quickEmoji = (emoji: EmojiProvider) => (property: keyof Emoji, destination: EmojiDestination) => {
+export const quickEmoji = (emoji: EmojiProvider) => {
 	const items: QuickPickItem[] = Array.from(emoji.emojis).map(
-		emoji => ({label: emoji.name, description: emoji.emoji})
+		emoji => ({ description: emoji.name, label: emoji.emoji })
 	);
-	return async function () {
-		const terminal = window.activeTerminal;
-		const editor = window.activeTextEditor;
-		const emoji = await window.showQuickPick(items);
-		if (!emoji) return;
-		const insert = property === 'name' ? `:${emoji.label}:`: emoji.description || '';
-		if (terminal && destination === 'terminal') terminal.sendText(insert, false);
-		else if (editor && destination === 'editor') {
-			editor.edit(builder => builder.insert(editor.selection.active, insert));
+	return (property: keyof Emoji, destination: EmojiDestination) => {
+		return async function () {
+			const terminal = window.activeTerminal;
+			const editor = window.activeTextEditor;
+			const emoji: QuickPickItem | undefined = await new Promise<QuickPickItem>(resolve => {
+				const picker = window.createQuickPick<QuickPickItem>()
+				picker.items = items;
+				picker.matchOnDescription = true;
+				picker.placeholder = 'Your Emojy Code';
+				picker.title = property === 'emoji' ? 'Emoji Picker' : 'Emoji Code Picker';
+				picker.onDidAccept(() => {
+					picker.hide()
+					resolve(picker.selectedItems[0]);
+				});
+				picker.onDidHide(() => {
+					picker.dispose()
+					resolve(undefined);
+				});
+				picker.show();
+			});
+			if (!emoji) return;
+
+			const insert = property === 'name' ? `:${emoji.description}:` : emoji.label || '';
+			if (terminal && destination === 'terminal') {
+				terminal.sendText(insert, false);
+			}
+			else if (editor && destination === 'editor') {
+				editor.edit(builder => builder.insert(editor.selection.active, insert));
+			}
 		}
 	}
 };
