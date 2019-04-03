c54a7290487dde86948ffd25cf9c40d69ec31985
Matt Bierner <mattbierner@gmail.com>
2019-04-03 10:10:33 -0700

    Fix tslint

--- src/extension.ts
+++ src/extension.ts
@@ -1,51 +1,46 @@
-import * as vscode from 'vscode'
-import EmojiCompletionProvider from './EmojiCompletionProvider'
-import { EmojiProvider } from './emoji'
-import Configuration from './configuration'
+import * as vscode from 'vscode';
+import Configuration from './configuration';
 import DecoratorProvider from "./DecoratorProvider";
+import { EmojiProvider } from './emoji';
+import EmojiCompletionProvider from './EmojiCompletionProvider';
 import { quickEmoji } from "./quickEmoji";
 
 function registerProviders(
     provider: EmojiCompletionProvider,
-    config: Configuration
+    config: Configuration,
 ): vscode.Disposable {
-    const disposables: vscode.Disposable[] = []
+    const disposables: vscode.Disposable[] = [];
     for (const language of config.languages) {
         if (config.shouldShowOnColon(language)) {
-            disposables.push(vscode.languages.registerCompletionItemProvider(language, provider, ':'))
+            disposables.push(vscode.languages.registerCompletionItemProvider(language, provider, ':'));
         } else {
-            disposables.push(vscode.languages.registerCompletionItemProvider(language, provider))
+            disposables.push(vscode.languages.registerCompletionItemProvider(language, provider));
         }
     }
 
     return vscode.Disposable.from(...disposables);
 }
 
-
 export function activate(context: vscode.ExtensionContext) {
-    const emoji = new EmojiProvider()
-    const config = new Configuration()
-    const provider = new EmojiCompletionProvider(emoji, config)
+    const emoji = new EmojiProvider();
+    const config = new Configuration();
+    const provider = new EmojiCompletionProvider(emoji, config);
 
-    let providerSub = registerProviders(provider, config)
+    let providerSub = registerProviders(provider, config);
+
     const emojiPicker = quickEmoji(emoji);
     context.subscriptions.push(
-        vscode.commands.registerCommand('emojisense.quickEmoji', emojiPicker('emoji', 'editor'))
+        vscode.commands.registerCommand('emojisense.quickEmoji', emojiPicker('emoji', 'editor')),
+        vscode.commands.registerCommand('emojisense.quickEmojitext', emojiPicker('name', 'editor')),
+        vscode.commands.registerCommand('emojisense.quickEmojiTerminal', emojiPicker('emoji', 'terminal')),
+        vscode.commands.registerCommand('emojisense.quickEmojitextTerminal', emojiPicker('name', 'terminal')),
     );
-    context.subscriptions.push(
-        vscode.commands.registerCommand('emojisense.quickEmojitext', emojiPicker('name', 'editor'))
-    );
-    context.subscriptions.push(
-        vscode.commands.registerCommand('emojisense.quickEmojiTerminal', emojiPicker('emoji', 'terminal'))
-    );
-    context.subscriptions.push(
-        vscode.commands.registerCommand('emojisense.quickEmojitextTerminal', emojiPicker('name', 'terminal'))
-    );
-    vscode.workspace.onDidChangeConfiguration(() => {
-        config.updateConfiguration()
-        providerSub.dispose()
-        providerSub = registerProviders(provider, config)
-    }, null, context.subscriptions)
 
-    context.subscriptions.push(new DecoratorProvider(emoji, config))
+    vscode.workspace.onDidChangeConfiguration(() => {
+        config.updateConfiguration();
+        providerSub.dispose();
+        providerSub = registerProviders(provider, config);
+    }, null, context.subscriptions);
+
+    context.subscriptions.push(new DecoratorProvider(emoji, config));
 }
--- src/quickEmoji.ts
+++ src/quickEmoji.ts
@@ -1,41 +1,43 @@
-import { Uri, window, Disposable, QuickPickItem, workspace, TextEdit, commands } from 'vscode';
-import { EmojiProvider, Emoji } from './emoji'
+import { commands, QuickPickItem, window } from 'vscode';
+import { Emoji, EmojiProvider } from './emoji';
 
 export type EmojiDestination = 'terminal' | 'editor';
-export const quickEmoji = (emoji: EmojiProvider) => {
-	const items: QuickPickItem[] = Array.from(emoji.emojis).map(
-		emoji => ({ description: emoji.name, label: emoji.emoji })
-	);
-	return (property: keyof Emoji, destination: EmojiDestination) => {
-		return async function () {
-			const terminal = window.activeTerminal;
-			const editor = window.activeTextEditor;
-			const emoji: QuickPickItem | undefined = await new Promise<QuickPickItem>(resolve => {
-				const picker = window.createQuickPick<QuickPickItem>()
-				picker.items = items;
-				picker.matchOnDescription = true;
-				picker.placeholder = 'Your Emojy Code';
-				picker.title = property === 'emoji' ? 'Emoji Picker' : 'Emoji Code Picker';
-				picker.onDidAccept(() => {
-					picker.hide()
-					resolve(picker.selectedItems[0]);
-				});
-				picker.onDidHide(() => {
-					picker.dispose()
-					resolve(undefined);
-				});
-				picker.show();
-			});
-			if (!emoji) return;
+export const quickEmoji = (provider: EmojiProvider) => {
+    const items: QuickPickItem[] = Array.from(provider.emojis).map((emoji) => {
+        return ({ description: emoji.name, label: emoji.emoji });
+    });
 
-			const insert = property === 'name' ? `:${emoji.description}:` : emoji.label || '';
-			if (terminal && destination === 'terminal') {
-				terminal.sendText(insert, false);
-				await commands.executeCommand('workbench.action.terminal.focus');	
-			}
-			else if (editor && destination === 'editor') {
-				editor.edit(builder => builder.insert(editor.selection.active, insert));
-			}
-		}
-	}
+    return (property: keyof Emoji, destination: EmojiDestination) => {
+        return async () => {
+
+            const emoji: QuickPickItem | undefined = await new Promise<QuickPickItem>((resolve) => {
+                const picker = window.createQuickPick<QuickPickItem>();
+                picker.items = items;
+                picker.matchOnDescription = true;
+                picker.placeholder = 'Your Emoji Code';
+                picker.title = property === 'emoji' ? 'Emoji Picker' : 'Emoji Code Picker';
+                picker.onDidAccept(() => {
+                    picker.hide();
+                    resolve(picker.selectedItems[0]);
+                });
+                picker.onDidHide(() => {
+                    picker.dispose();
+                    resolve(undefined);
+                });
+                picker.show();
+            });
+            if (!emoji) {
+                return;
+            }
+
+            const insert = property === 'name' ? `:${emoji.description}:` : emoji.label || '';
+            if (window.activeTerminal && destination === 'terminal') {
+                window.activeTerminal.sendText(insert, false);
+                await commands.executeCommand('workbench.action.terminal.focus');
+            } else if (window.activeTextEditor && destination === 'editor') {
+                const editor = window.activeTextEditor;
+                editor.edit((builder) => builder.insert(editor.selection.active, insert));
+            }
+        };
+    };
 };
