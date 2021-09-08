b79bf5ee8d98cee8e9b266f5309abae41a3541eb
Matt Bierner <matb@microsoft.com>
2021-09-07 17:06:24 -0700

    Update gemoji

--- src/emoji.ts
+++ src/emoji.ts
@@ -1,3 +1,5 @@
+import { gemoji } from 'gemoji';
+
 export interface Emoji {
     readonly name: string;
     readonly emoji: string;
@@ -20,13 +22,11 @@ export class EmojiProvider {
 
     private get emojiMap(): Map<string, Emoji> {
         if (!this._emojiMap) {
-            // eslint-disable-next-line @typescript-eslint/no-var-requires
-            const gemojies = require('gemoji');
             this._emojiMap = new Map<string, Emoji>();
-            for (const gemoji of gemojies) {
-                for (const name of gemoji.names) {
+            for (const g of gemoji) {
+                for (const name of g.names) {
                     if (!this._emojiMap.has(name)) {
-                        this._emojiMap.set(name, { name, emoji: gemoji.emoji });
+                        this._emojiMap.set(name, { name, emoji: g.emoji });
                     }
                 }
             }
